81aebe79f43b7af38328bde1cedd30f28447cb99
MontiPGib <131723953+MontiPGib@users.noreply.github.com>
2024-11-03 20:11:37 -0300

    Añadir función de cerrar sesión

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const Profile = ({ reservations }) => {
+const Profile = ({ reservations, username, setLoggedIn }) => {
+  const navigate = useNavigate();
+
+  const cerrarSesion = () => {
+    alert(`Se ha cerrado la sesión de ${username}`);
+    reservations.length = 0;
+    setLoggedIn(false);
+    navigate('/login');
+  };
+
   return (
     <div>
       <h2>Mi Perfil</h2>
@@ -10,6 +20,7 @@ const Profile = ({ reservations }) => {
           <li key={index}>{reservation}</li>
         ))}
       </ul>
+      <button onClick={cerrarSesion}>Cerrar Sesión</button>
     </div>
   );
 };
